b00d77ca734d3e6caea054b2e451ae5639032190
JIoxmydak <6aneme@gmail.com>
2023-12-21 18:57:47 +0300

    Added testing and logging

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
